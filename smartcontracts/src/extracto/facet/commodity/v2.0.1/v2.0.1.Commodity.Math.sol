19ee572dbfb45869269cf2e1320bdc24245e7b0d
Robson <robson.miranda1991@gmail.com>
2023-11-09 14:59:03 -0300

    fix commodity functions

--- smartcontracts/src/extracto/facet/commodity/v2.0.1/v2.0.1.Commodity.Math.sol
+++ smartcontracts/src/extracto/facet/commodity/v2.0.1/v2.0.1.Commodity.Math.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-import {div, ud60x18, unwrap, UD60x18, mul, floor} from "../../../../utils/math/UD60x18.sol";
+import {div, ud60x18, unwrap, UD60x18, mul, floor, add} from "../../../../utils/math/UD60x18.sol";
 import {CommodityStorageLib} from "../../../diamond/libraries/Lib.Commodity.sol";
 import {UD60x18} from "../../../../utils/math/Type.sol";
 import {AuthV201} from "./v2.0.1.Commodity.Auth.sol";
@@ -43,6 +43,35 @@ abstract contract MathV201 is AuthV201 {
         // getTotalSupplyKG - amount
         lib.totalCommoditySupply = unwrap(
             ud60x18(lib.totalCommoditySupply).sub(ud60x18(amount))
+        );
+    }
+
+    function calculateBlockTarget(uint256 currentBlock, uint256 locktime)  internal pure returns (uint256) {
+        return unwrap(add(ud60x18(currentBlock), ud60x18(locktime)));
+    }
+
+    function calculateSellAmountYielded(uint256 kg) internal view returns (uint256) {
+        CommodityStorageLib.Storage storage lib = CommodityStorageLib.getCommodityStorage();
+
+        // yieldedKg = (kg * (1 + yieldFarming/100))
+        // betterPrecisionYieldKd = yieldedKg / 1^18
+        // kgInDolar = betterPrecisionYieldKd * weightPrice
+        // kgInCow = kgInDolar / 0.1
+
+        UD60x18 COW_TOKEN_PRICE_IN_DOLAR = div(ud60x18(1), ud60x18(10));
+        uint256 BETTER_PRECISION = 1e18;
+        uint256 PERCENTAGE = 100;
+        return unwrap(
+            mul(
+                mul(
+                    div(
+                        div(mul(ud60x18(kg), ud60x18(lib.yieldFarming)), ud60x18(PERCENTAGE)).add(ud60x18(kg)),
+                        ud60x18(BETTER_PRECISION)
+                    ),
+                    ud60x18(lib.sellPrice)
+                ),
+                COW_TOKEN_PRICE_IN_DOLAR
+            )
         );
     }
 }
